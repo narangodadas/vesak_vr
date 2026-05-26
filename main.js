88dbbd61a05fb88bc892f4a6982c4a652ffa6d23
sahannarangoda <narangodadas@gmail.com>
2026-05-27 02:57:08 +0530

    uhgfg

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ const anchorState = {
   anchorX: 0,
   anchorY: -1.0,
   anchorZ: -3.0,
-  scale: 0.2
+  scale: 0.35
 };
 
 startBtn.addEventListener("click", async () => {
@@ -239,7 +239,7 @@ function placeLanternAtQRLocation(qrText, qrLocation) {
 
   anchorState.anchorY = -1.0;
   anchorState.anchorZ = -3.0;
-  anchorState.scale = 0.2;
+  anchorState.scale = 0.35;
 
   camera.position.set(0, 0, 0);
   camera.rotation.set(0, 0, 0);
@@ -330,7 +330,7 @@ function resetAnchor() {
   anchorState.anchorX = 0;
   anchorState.anchorY = -1.0;
   anchorState.anchorZ = -3.0;
-  anchorState.scale = 0.2;
+  anchorState.scale = 0.35;
 
   stopVesakMusic();
 
