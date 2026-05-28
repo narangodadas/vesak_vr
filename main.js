232d9772924cb75363808da792feca54095fceed
sahannarangoda <narangodadas@gmail.com>
2026-05-29 01:16:24 +0530

    ggv

--- main.js
+++ main.js
@@ -334,7 +334,7 @@ function placeAnchor(qrText, qrLocation) {
     anchor.position.y + 0.5,      // float above the QR code
     anchor.position.z
   );
-  currentLantern.scale.setScalar(0.52);
+  currentLantern.scale.setScalar(0.48);
   scene.add(currentLantern);
 
   // Ground shadow sits on the floor directly under the lantern
